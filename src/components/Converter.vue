a4d4ad8ba3ce7a56276a8ce74d00edf3b8e4756c
LarryBits <olanlokunlanre@gmail.com>
2019-01-25 17:39:09 +0100

    Initial Commit

---
+++ src/components/Converter.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <div id="jumb" class="row justify-content-center">
+      <div class="col-lg-4 col-md-4 col-sm-4">             
+        <input type="text" v-model ='boxOne' id ="basic-addon2nd" required=true  class=" form-control rounded-0" placeholder="Crypto QTY" aria-label="Username" aria-describedby="basic-addon1">
+      </div><br>
+      <div class="col-lg-4 col-md-4 col-sm-4">
+        <select class="btn w-100 h-100" @change="getData">
+          <option>Select Coin</option>
+          <option :key="short.id" v-for="short in shortCode" :value="short" >{{short}}</option>
+        </select>
+      </div><br>
+      <div class="col-lg-4 col-md-4">
+        <input type="text" id ="basic-addon2nd" class=" form-control rounded-0"  disabled :placeholder='boxTwo'>
+      </div>
+      <footer>
+        <h4 id='foot'>&copy; {{date.getFullYear()}} LarryBits </h4>
+      </footer>
+    </div>
+  
+</div>
+</template>
+<script>
+  import {coin} from '../assets/Coin.js'
+export default {
+  data(){
+    return{
+      date: new Date(),
+      shortCode:coin,
+      boxOne:1,
+      boxTwo:'NGN',
+      coinPrice:'',
+      usdPrice:''
+    }
+  },
+
+ mounted(){
+   this.getUsdNaira();
+ },
+ watch:{
+   boxOne(newValue,oldValue){
+     this.boxTwo = `#${(newValue * parseFloat(this.coinPrice.price) * this.usdPrice.val).toFixed(4)}`;
+   }
+},
+  methods:{
+    //Function get called on Mounted to get dollar-Naira rate
+    getUsdNaira(){
+     axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=USD_NGN&compact=y`) 
+      .then(result=>{
+        this.usdPrice = {...result.data.USD_NGN}
+      })
+     
+    },
+  // Function get called onSelecting new option/coin
+    getData(event){
+      let val = event.target.value.toLowerCase();
+      axios.get(`https://api.cryptonator.com/api/ticker/${val}-usd`)
+        .then(response=>{
+          this.coinPrice = {...response.data.ticker};
+          this.boxTwo =`#${(this.boxOne * parseFloat(this.coinPrice.price) * this.usdPrice.val).toFixed(4)}`;
+        })
+
+        
+    }
+   
+   } 
+  
+  }
+  
+</script>
+
+<style scoped>
+  #jumb{
+    margin-top: 10em;
+  }
+ #basic-addon2nd{
+    padding: 2em;
+    
+  }
+#basic-addon2nd::placeholder{
+  color:#143441;
+  font-family: 'Titillium Web', sans-serif;
+  font-size:1.5em;
+}
+input{
+  color:#143441;
+  font-family: 'Titillium Web', sans-serif;
+  font-size:1.4em;
+}
+select{
+  border:1px solid #143441;
+  color:#0D1F21;
+  font-family: 'Titillium Web', sans-serif;
+  font-size:1.5em;
+}
+
+footer{
+  margin-top:100em;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="content shadow">
+     <h3 class="display-5 text-center py-3">Simple Crypto Converter</h3>
+                        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    h3{
+        font-family: 'Comfortaa', cursive;
+        color:#143441;
+    }
+
+
+</style>
